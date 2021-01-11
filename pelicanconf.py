37489a15e2fb12642b79a2c26d2d82a0e53871d6
chemacortes <devxtrem@gmail.com>
2021-01-11 03:49:00 +0100

    Añadido plugin mdx_include para markdown

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Chema Cortés'
-SITENAME = 'Hyperreals *R'
+AUTHOR = "Chema Cortés"
+SITENAME = "Hyperreals *R"
 SITESUBTITLE = "Quarks, bits y otras criaturas infinitesimales"
-GITHUB_URL = 'http://github.com/chemacortes/blog'
-SITEURL = ''
+GITHUB_URL = "http://github.com/chemacortes/blog"
+SITEURL = ""
 
-PATH = 'content'
-TIMEZONE = 'Europe/Paris'
-DEFAULT_LANG = 'es'
+PATH = "content"
+TIMEZONE = "Europe/Paris"
+DEFAULT_LANG = "es"
 
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = SITEURL
@@ -24,23 +24,22 @@ FEED_MAX_ITEMS = 30
 
 # Blogroll
 LINKS = (
-    ('All Feeds', '/feeds/all.atom.xml'),
-    ('Scala Feeds', '/feeds/scala.atom.xml'),
-    ('Python Feeds', '/feeds/python.atom.xml'),
+    ("All Feeds", "/feeds/all.atom.xml"),
+    ("Scala Feeds", "/feeds/scala.atom.xml"),
+    ("Python Feeds", "/feeds/python.atom.xml"),
     # ('Haskell Feeds','/feeds/haskell.atom.xml'),
-    ('my coursera',
-     'https://www.coursera.org/user/9408450118c4cfeddff015451ed358b6'),
+    ("my coursera", "https://www.coursera.org/user/9408450118c4cfeddff015451ed358b6"),
 )
 
 # Social widget
 SOCIAL = (
-    ('twitter', 'https://twitter.com/chemacortes'),
-    ('github', 'https://github.com/chemacortes'),
-    ('bitbucket', 'https://bitbucket.org/chemacortes/'),
-    ('stackoverflow', 'http://stackoverflow.com/users/1243400/chemacortes'),
+    ("twitter", "https://twitter.com/chemacortes"),
+    ("github", "https://github.com/chemacortes"),
+    ("bitbucket", "https://bitbucket.org/chemacortes/"),
+    ("stackoverflow", "http://stackoverflow.com/users/1243400/chemacortes"),
     # ('delicious', 'https://delicious.com/chemacortes'),
     # ('google+', 'https://plus.google.com/+ChemaCortés'),
-    ('linkedin', 'https://linkedin.com/in/chemacortes'),
+    ("linkedin", "https://linkedin.com/in/chemacortes"),
     # ('facebook', 'https://facebook.com/pych3m4'),
     # ('flickr', 'https://www.flickr.com/photos/chemacortes'),
 )
@@ -53,46 +52,40 @@ RELATIVE_URLS = True
 
 TYPOGRIFY = True
 
-STATIC_PATHS = [
-    "extra",
-    "pictures",
-    "code"
-]
+STATIC_PATHS = ["extra", "pictures", "code"]
 EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/README.md': {'path': 'README.md'},
-    'extra/CNAME': {'path': 'CNAME'},
-    'extra/google989145ab610d7f0b.html': {
-        'path': 'google989145ab610d7f0b.html'
-    },
+    "extra/robots.txt": {"path": "robots.txt"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "extra/README.md": {"path": "README.md"},
+    "extra/CNAME": {"path": "CNAME"},
+    "extra/google989145ab610d7f0b.html": {"path": "google989145ab610d7f0b.html"},
 }
-IGNORE_FILES = ['extra']
+IGNORE_FILES = ["extra"]
 
 INDEX_SAVE_AS = "index.html"
 ARCHIVES_URL = "archives.html"
-ARCHIVES_SAVE_AS = 'archives.html'
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
-DRAFT_URL = 'drafts/{date:%Y}-{date:%m}-{date:%d}-{slug}.html'
-DRAFT_SAVE_AS = 'drafts/{date:%Y}-{date:%m}-{date:%d}-{slug}.html'
-PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
+ARCHIVES_SAVE_AS = "archives.html"
+ARTICLE_URL = "{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
+DRAFT_URL = "drafts/{date:%Y}-{date:%m}-{date:%d}-{slug}.html"
+DRAFT_SAVE_AS = "drafts/{date:%Y}-{date:%m}-{date:%d}-{slug}.html"
+PAGE_URL = "pages/{slug}/"
+PAGE_SAVE_AS = "pages/{slug}/index.html"
 
-TAGS_URL = 'tag/'
-TAGS_SAVE_AS = 'tag/index.html'
-TAG_URL = 'tag/{slug}/'
-TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_URL = "tag/"
+TAGS_SAVE_AS = "tag/index.html"
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = "tag/{slug}/index.html"
 
-CATEGORIES_URL = 'category/'
-CATEGORIES_SAVE_AS = 'category/index.html'
-CATEGORY_URL = 'category/{slug}/'
-CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_URL = "category/"
+CATEGORIES_SAVE_AS = "category/index.html"
+CATEGORY_URL = "category/{slug}/"
+CATEGORY_SAVE_AS = "category/{slug}/index.html"
 
-AUTHORS_URL = ''  # 'blog/authors/'
-AUTHORS_SAVE_AS = ''  # 'blog/authors/index.html'
-AUTHOR_URL = ''  # 'blog/authors/{slug}.html'
-AUTHOR_SAVE_AS = ''  # 'blog/authors/{slug}.html'
+AUTHORS_URL = ""  # 'blog/authors/'
+AUTHORS_SAVE_AS = ""  # 'blog/authors/index.html'
+AUTHOR_URL = ""  # 'blog/authors/{slug}.html'
+AUTHOR_SAVE_AS = ""  # 'blog/authors/{slug}.html'
 
 
 THEME = "pelican-subtle-mod"
@@ -107,56 +100,48 @@ PLUGINS = [
     "render_math",
     "sitemap",
     "neighbors",
-    'liquid_tags',
-    'liquid_tags.img',
-    'liquid_tags.audio',
-    'liquid_tags.video',
-    'liquid_tags.youtube',
-    'liquid_tags.vimeo',
-    'liquid_tags.include_code',
-    'plantuml'
+    "liquid_tags",
+    "liquid_tags.img",
+    "liquid_tags.audio",
+    "liquid_tags.video",
+    "liquid_tags.youtube",
+    "liquid_tags.vimeo",
+    "liquid_tags.include_code",
+    "plantuml",
 ]
 # DIRECT_TEMPLATES = ('index', 'tags', 'categories','archives')
 
 MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.codehilite': {
-            'css_class': 'highlight',
-            'guess_lang': False,
-            'linenums': True,
+    "extension_configs": {
+        "mdx_include": {},
+        "markdown.extensions.codehilite": {
+            "css_class": "highlight",
+            "guess_lang": False,
+            "linenums": True,
         },
-        'markdown.extensions.extra': {},
-        'markdown.extensions.meta': {},
-        'markdown.extensions.admonition': {},
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+        "markdown.extensions.admonition": {},
     },
-    'output_format': 'html5',
+    "output_format": "html5",
 }
 
 
 SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.9,
-        'indexes': 0.5,
-        'pages': 0.3
-    },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'weekly',
-        'pages': 'yearly'
-    }
+    "format": "xml",
+    "priorities": {"articles": 0.9, "indexes": 0.5, "pages": 0.3},
+    "changefreqs": {"articles": "monthly", "indexes": "weekly", "pages": "yearly"},
 }
 
 MATH_JAX = {
-    'equation_numbering': "AMS",
+    "equation_numbering": "AMS",
 }
 
 # Theme specific
 DISPLAY_META_ABOVE_ARTICLE = True
 FUZZY_DATES = True
 TAGLINE = "Quarks, bits y otras criaturas infinitesimales"
-USER_LOGO_URL = \
-    "https://s.gravatar.com/avatar/b20d114964d6d77c209aadbe9a152e87?s=80"
+USER_LOGO_URL = "https://s.gravatar.com/avatar/b20d114964d6d77c209aadbe9a152e87?s=80"
 # DISQUS_SITENAME = "hyperreals"
 # DISQUS_COLLAPSED = True
 GLOBAL_KEYWORDS = [
@@ -164,5 +149,5 @@ GLOBAL_KEYWORDS = [
     "programming",
     "programación",
     "lambda calculus",
-    "math"
+    "math",
 ]
